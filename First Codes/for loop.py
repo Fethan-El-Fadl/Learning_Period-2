5b454a54c48eb5d91e77c548569282adc60f9eee
Jaaku-Kun <fahimkhan1785@gmail.com>
2021-06-09 15:02:03 +0600

    for loop

---
+++ First Codes/for loop.py
@@ -0,0 +1,15 @@
+#for i in range(10):
+    #print(i+1)
+
+#for i in range(50,100+1,2)
+    #print(i)
+#for i in "Fahim Khan":
+    #print(i)
+
+import time 
+
+for seconds in range(10,0,-1):
+    print(seconds)
+    time.sleep(1)
+print("Happy New Year!")
+
