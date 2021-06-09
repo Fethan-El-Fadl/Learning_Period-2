4f9bb44e95b7cb6cb378c31b962a64a6bd549041
Jaaku-Kun <fahimkhan1785@gmail.com>
2021-06-09 15:27:57 +0600

    bcp

---
+++ First Codes/break continue pass ⛔.py
@@ -0,0 +1,23 @@
+#while True:
+    #name = input("Enter your name: ")
+    #if name != "":
+        #break
+
+#phone_number = "123-456-7890"
+
+#for i in phone_number:
+    #if i == "-":
+        #continue
+    #print(i, end="")
+
+for i in range(1,21):
+
+    if i == 13:
+        pass
+    else:
+        print(i)
+        
+
+
+
+
