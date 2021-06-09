0757fdfa974ebbe653be50a52fc1f995bff3236b
Jaaku-Kun <fahimkhan1785@gmail.com>
2021-06-09 14:34:43 +0600

    all set

--- First Codes/Logical Operator.py
+++ First Codes/Logical Operator.py
@@ -1,12 +1,14 @@
 temp = int(input("What is the temperature outside?: "))
 
-if temp >= 0 and temp <= 30:
-    print("The temperature is good today!")
-    print("Go outside!")
-elif temp < 0 or temp > 30:
+if not(temp >= 0 and temp <= 30):
     print("The temparature is bad today.")
     print("Stay inside!")
-    print("Hi!")
+
+elif not(temp < 0 or temp > 30):
+    print("The temperature is good today!")
+    print("Go outside!")
+
+    
 
 
 
