8786984b91d6abb592d1263896ae50799f52e243
Jaaku-Kun <fahimkhan1785@gmail.com>
2021-06-14 16:01:19 +0600

    foods!

--- First Codes/lists🧾.py
+++ First Codes/lists🧾.py
@@ -1,1 +1,14 @@
-#I' Fahim
+food = ["pizza" , "hamburger" , "hotdog" , "spaghetti" , "pudding"]
+
+food[0] = "sushi"
+#print(food[0])
+#food.append("ice cream")
+#food.remove("Hotdog")
+#food.pop()
+#food.insert(0,"cake")
+#food.sort()
+#food.clear()
+
+
+for x in food:
+    print(x)
